b03a2bd00665bf95c810367432e3486ce2d743ef
designermoth <ruben.guilherme.dev@gmail.com>
2025-01-26 09:47:42 +0000

    Added Map

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource audioSource;
+
+    public void OnStartButton()
+    {
+        audioSource.Play();
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButton()
+    {
+        audioSource.Play();
+        Application.Quit();
+    }
+}
